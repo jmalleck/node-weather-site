8c4445eadb8a2a95c7d9771106e5f50b30a17466
Joel Malleck <joelmalleck@gmail.com>
2020-06-26 17:43:54 -0400

    Update forecast info and units

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,14 +1,14 @@
 const request = require('request');
 
 const forecast = (lat, lon, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=9246a7d2f3168ba73dd08087daac118f&query=' + lat + ',' + lon + '&units=f';
+    const url = 'http://api.weatherstack.com/current?access_key=9246a7d2f3168ba73dd08087daac118f&query=' + lat + ',' + lon;
     request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service!');
         } else if (body.error) {
             callback('Unable to find location!');
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike +' degrees out.');
+            callback(undefined, body.current.weather_descriptions[0] + '.\nIt is currently ' + body.current.temperature + ' degrees out, and it feels like ' + body.current.feelslike +' degrees out. The humidity is ' + body.current.humidity + ' percent. The wind speed is ' + body.current.wind_speed + ' km/h.');
         }
     });
 };
